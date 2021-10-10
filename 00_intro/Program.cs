eaa89b2158b335f769e07e492c10db2362e7c3a3
Keith <keith.ping@gmail.com>
2021-10-10 12:33:48 -0400

    Initial commit

---
+++ 00_intro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //What is C#?
+            //C# is an Object Oriented Programming Language
+            //The universal term for an object in C# is POCO
+            //POCO stands for Plain Old CLR Object
+            //CLR stands for Common Language Runtime
+            //Methods (sometimes called functions) will be behaviors of an object
+            //Properties will be nouns of an object
+            //There are four pillars of an object oriented programming language:
+            //Encapsulation
+                //access modifiers
+                //the most important part of Encapsulation is privacy (like public or private)
+            //Abstraction
+            //Inheritance
+            //Polymorphismca
+        }
+    }
+}
---
+++ 01_ValueTypes/ValueTypes.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _01_ValueTypes
+{
+    public enum PastryTypes
+    {
+        Cake = 0,
+        Danish,
+        Donut
+    }
+
+    [TestClass]
+    public class ValueTypes
+    {
+        [TestMethod]
+        public void InitAndDeclareVariables()
+        {
+            //Declaring a Variable
+            int numOne;
+            numOne = 1;
+
+            //Declaring and Initializing a Variable
+            int numTwo = 2;
+
+            //A Variable is a named location in computer memory (AKA RAM)
+        }
+
+        [TestMethod]
+        public void  ValueTypeVariables()
+        {
+            //Whole Numbers
+            byte byteExample = 255;
+            sbyte sByteExample = -128;
+            short shortExample = 32165;
+            Int16 anotherShortExample = 32000;
+            int intMin = -2147483648;
+            int intMax = 2147483647;
+            Int32 anotherIntExample = 123456;
+            long longMax = 9223372036854775807;
+            Int64 longMin = -9223372036854775808;
+            
+            //Decimals
+            //A Double can also end with the suffix d, but is not required to by default
+            //A Float must end with the suffix f
+            //A Decimal must end with the suffix m
+            double doubleExample = 1.123456789;
+            float floatExample = 1.123456789f;
+            decimal decimalExample = 1.11223546657489m;
+
+            Console.WriteLine(doubleExample);
+
+            //Character
+            char charExample = 'a';
+            char number = '3';
+            char symbol = '&';
+            char space = ' ';
+            char specialChar = '\n';
+
+            //Bool
+            bool isHungry = true;
+            bool isTired = false;
+
+
+            //Enum
+            PastryTypes myPastry = PastryTypes.Donut;
+
+            //Struts
+            DateTime today = DateTime.Now;
+            Console.WriteLine(today);
+
+        }
+    }
+}
